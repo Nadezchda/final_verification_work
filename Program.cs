130da05f8a74ef049740bf888515d796c163aa2b
Nadezchda <107324838+Nadezchda@users.noreply.github.com>
2022-08-07 20:53:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+// ЗАДАЧА:
+// Написать программу, которая из имеющегося массива строк
+// формирует массивы из строк, длина которых меньше либо равна 3 символа.
+// Первоначальный массив можно ввести с кулавиатуры либо задать на старте
+// выполнения алгоритма.
+
+ string[] array1 = new string[4] {"3456", "678", "hello", ":-)"};
+ Console.Write("[3456, " + "678, " + "hello, "+ ":-)] => [");
+
+ string[] array2 = new string[array1.Length];
+
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}" + ",");
+    }
+    Console.WriteLine("]");
+}
+SecondArray(array1 , array2);
+PrintArray(array2);
